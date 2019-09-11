902365f07d2f8a7d801fa5f77fd9fdbc0c0462eb
thaigialai1987 <thaigialai1987@gmail.com>
2019-09-11 12:18:50 +0700

    first project

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * First, we will load all of this project's Javascript utilities and other
+ * dependencies. Then, we will be ready to develop a robust and powerful
+ * application frontend using useful Laravel and JavaScript libraries.
+ */
+
+import './bootstrap';
+import Vue from 'vue';
+import Vuetify from "vuetify";
+
+
+// Route
+import Routes from'@/js/routes.js';
+
+// Compoment file
+
+
